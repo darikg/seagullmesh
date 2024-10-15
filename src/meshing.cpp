12ea980cb6476dc281b9991e3c2432b2f7f3169c
Darik Gamble <darik.gamble@gmail.com>
2024-10-15 10:42:03 -0400

    ok...

--- src/meshing.cpp
+++ src/meshing.cpp
@@ -195,15 +195,38 @@ void init_meshing(py::module &m) {
     ;
 
     py::class_<AdaptiveSizingField>(sub, "AdaptiveSizingField", py::module_local())
-        .def(py::init([](
-            const double tol,
-            const std::pair<double, double>& edge_len_min_max,
-            const Faces& faces,
-            Mesh3& mesh,
-            double ball_radius
-        ) {
-            auto params = PMP::parameters::ball_radius(ball_radius);
-            return AdaptiveSizingField(tol, edge_len_min_max, faces, mesh, params);
-        }))
+        .def(
+            py::init([](
+                const double tol,
+                const std::pair<double, double>& edge_len_min_max,
+                const Faces& faces,
+                Mesh3& mesh,
+            ) {
+                return AdaptiveSizingField(tol, edge_len_min_max, faces, mesh);
+            })
+//            ,
+//            "An adaptive sizing field for curvature-based remeshing"
+//            py::arg("tol"),
+//            py::arg("edge_len_min_max"),
+//            py::arg("faces"),
+//            py::arg("mesh"),
+        )
+//        .def(
+//            py::init([](
+//                const double tol,
+//                const std::pair<double, double>& edge_len_min_max,
+//                const Faces& faces,
+//                Mesh3& mesh,
+//                double ball_radius
+//            ) {
+//                return AdaptiveSizingField(tol, edge_len_min_max, faces, mesh, PMP::parameters::ball_radius(ball_radius));
+//            }),
+//            "An adaptive sizing field for curvature-based remeshing"
+//            py::arg("tol"),
+//            py::arg("edge_len_min_max"),
+//            py::arg("faces"),
+//            py::arg("mesh"),
+//            py::arg("ball_radius"),
+//        )
     ;
 }
