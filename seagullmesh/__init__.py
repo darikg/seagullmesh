4f2b253563cb0e5b1cefcd61890e402d04189dcf
Darik Gamble <darik.gamble@gmail.com>
2024-10-20 08:14:23 -0400

    more typing cleanup

--- seagullmesh/__init__.py
+++ seagullmesh/__init__.py
@@ -278,7 +278,7 @@ class Mesh3:
         """Returns True if the mesh self-intersects"""
         return sgm.meshing.does_self_intersect(self._mesh)
 
-    def aabb_tree(self, vert_points: Union[str, VertexPointMap] = 'points'):
+    def aabb_tree(self, vert_points: str | PropertyMap[Vertex, Point2 | Point3] = 'points'):
         """Construct an axis-aligned bounding box tree for accelerated point location by `Mesh3.locate_points
 
         By default, the AABB tree is constructed for the default mesh vertex locations, but also accepts a vertex
@@ -302,7 +302,7 @@ class Mesh3:
             self,
             faces: Faces,
             bary_coords: A,
-            vert_points: Union[str, VertexPointMap] = 'points',
+            vert_points: str | PropertyMap[Vertex, Point2 | Point3] = 'points',
     ) -> A:
         """Construct a set of points from face barycentric coordinates
 
@@ -331,13 +331,13 @@ class Mesh3:
         """
         return sgm.locate.shortest_path(self._mesh, src_face, src_bc, tgt_face, tgt_bc)
 
-    def lscm(self, uv_map: Union[str, UvMap]):
+    def lscm(self, uv_map: str | PropertyMap[Vertex, Point2]):
         """Performs least-squares conformal mapping"""
         if isinstance(uv_map, str):
             uv_map = self.vertex_data.get_or_create_property(uv_map, default=Point2(0, 0))
         sgm.parametrize.lscm(self._mesh, uv_map.pmap)
 
-    def arap(self, uv_map: Union[str, UvMap]):
+    def arap(self, uv_map: str | PropertyMap[Vertex, Point2]):
         """Performs as-rigid-as-possible parameterization"""
         uv_map = self.vertex_data.get_or_create_property(uv_map, default=Point2(0, 0))
         sgm.parametrize.arap(self._mesh, uv_map.pmap)
@@ -612,8 +612,6 @@ class ArrayPropertyMap(PropertyMap[Key, Val]):
         locals()[dunder] = _dunder_impl
 
 
-VertexPointMap = PropertyMap[Vertex, Union[Point2, Point3]]
-UvMap = PropertyMap[Vertex, Point2]
 
 
 class MeshData(Generic[Key]):
